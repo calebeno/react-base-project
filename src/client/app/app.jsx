d0592227423af57e63bdc4953e16a1350913ec64
Caleb Eno <ceno@icct.com>
2016-06-23 20:34:07 -0400

    Added basic routing

--- src/client/app/app.jsx
+++ src/client/app/app.jsx
@@ -1,18 +1,47 @@
 import React from 'react';
 import {render} from 'react-dom';
-import Counter from './components/counter/counter.jsx';
 import Hello from './components/hello/hello.jsx';
+import {Router, Route, Link, browserHistory, RouteHandler} from 'react-router'
 import './app.css';
 
-class App extends React.Component {
-  render () {
-    return (
-      <div>
-        <Hello />
-        <Counter />
-      </div>
-    );
-  }
+// class App extends React.Component {
+//   render () {
+//     return (
+//       <div>
+//         <Hello />
+//         <Counter />
+//       </div>
+//     );
+//   }
+// }
+
+class MainLayout extends React.Component {
+    render() {
+        return (
+            <div>
+                <span>Header:</span>
+                <hr/>
+                <div>
+                    <h2>Body Content</h2>
+                    {this.props.children}
+                </div>
+                <div>
+                    <hr/>
+                    footer
+                </div>
+            </div>
+        );
+    }
 }
 
-render(<App/>, document.getElementById('app'));
+
+render((
+    <Router>
+        <Route component={MainLayout}>
+            <Route path="/" component={Hello}/>
+        </Route>
+    </Router>
+), document.getElementById('app'));
+
+
+// render(<App/>, document.getElementById('app'));
--- src/client/app/components/hello/hello.jsx
+++ src/client/app/components/hello/hello.jsx
@@ -1,15 +1,17 @@
 'use strict';
 
 import React from 'react';
+import Counter from '../counter/counter.jsx';
 
 class Hello extends React.Component {
-  render() {
-    return (
-      <div className="hello">
-        Hello World!
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="hello">
+                Hello World!
+                <Counter />
+            </div>
+        );
+    }
 }
 
 export default Hello;
